56641ea68941defca248112497ed532f194d3ff2
josh <joshvin87@gmail.com>
2024-02-03 11:14:03 +0100

    a reto

--- App.java
+++ App.java
@@ -15,20 +15,51 @@
  * - También se debe proponer una operación de finalización del programa.
  * */
 
- import java.util.Arrays;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 public class App {
     public static void main(String[] args) throws Exception {
     //Colecciones
 
     //Arrays: son datos del mismo tipo y no se pueden añadir o eliminar
-    String cadenaTexto = "Pollo,pizza,patata,pasta,huevo";
-    String [] arrayCadenas = cadenaTexto.split(" ");
+    
+    String [] arrayCadenas = {"pollo","pizza","huevo","morcilla","pasta"};
     System.out.println(arrayCadenas[0]);
     System.out.println(arrayCadenas[1]);
     System.out.println(arrayCadenas[2]);
     System.out.println(arrayCadenas[3]);
     System.out.println(arrayCadenas[4]);
     Arrays.sort(arrayCadenas);
-    Arrays.asList(arrayCadenas).indexOf(4);
+    System.out.println(Arrays.asList(arrayCadenas).indexOf("morcilla"));
+
+    //Listas: son datos del mismo tipo y si se pueden añadir o eliminar
+
+    List<Integer> numbers = new ArrayList<Integer>(List.of(1,2,3,4));
+    System.out.println(numbers.get(0));
+    System.out.println(numbers.get(1));
+    System.out.println(numbers.get(2));
+    System.out.println(numbers.get(3));
+    numbers.set(0, 100);
+    numbers.add(387);
+    numbers.add(5,78);
+    numbers.size();
+    System.out.println(numbers.contains(387));
+    numbers.remove(1);
+    numbers.clear();
+
+    //Diccionarios: Compuestos por una clave y un valor asociado. La clave no puede repetirse.
+    HashMap<String, Integer> agendaContactos = new HashMap<String, Integer>();
+    agendaContactos.put("Andres", 676763434);
+    System.out.println(agendaContactos.size());
+    System.out.println(agendaContactos.get("Andres"));
+    System.out.println(agendaContactos.containsKey("Andres"));
+    System.out.println(agendaContactos.containsValue(23232442));
+    agendaContactos.remove("Andres");
+    System.out.println(agendaContactos.size());
+    agendaContactos.clear();
     }
+
+    
 }
