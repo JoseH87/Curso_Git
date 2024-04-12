7e3528072d690cb1b24d91b106850a9deb9b46e8
josh <joshvin87@gmail.com>
2024-04-12 13:40:49 +0200

    retomo reto

--- App.java
+++ App.java
@@ -59,7 +59,47 @@ public class App {
     agendaContactos.remove("Andres");
     System.out.println(agendaContactos.size());
     agendaContactos.clear();
+    
+    /* 
+     Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización
+ *   y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar,
+ *   y a continuación los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más
+ *   de 11 dígitos (o el número de dígitos que quieras).
+ * - También se debe proponer una operación de finalización del programa.
+ * */
+
+    System.out.println("Agenda de contactos");
+    System.out.println("Elija una opcion");
+    Integer option = Integer.parseInt(System.console().readLine());
+    switch (option) {
+        case 1:
+            System.out.println("Ingrese el nombre del contacto");
+            String name = System.console().readLine();
+            System.out.println("Ingrese el numero del contacto");
+            Integer number = Integer.parseInt(System.console().readLine());
+            agendaContactos.put(name, number);
+            break;
+        case 2:
+            System.out.println("Ingrese el nombre del contacto a eliminar");
+            String name2 = System.console().readLine();
+            agendaContactos.remove(name2);
+            break;
+        case 3:
+            System.out.println("Ingrese el nombre del contacto a modificar");
+            String name3 = System.console().readLine();
+            System.out.println("Ingrese el nuevo numero del contacto");
+            Integer number2 = Integer.parseInt(System.console().readLine());
+            agendaContactos.put(name3, number2);
+            break;
+            
+    
+        default:
+            break;
     }
 
-    
+    }
 }
